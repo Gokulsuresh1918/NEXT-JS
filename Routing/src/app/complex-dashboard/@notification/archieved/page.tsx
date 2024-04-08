7e0ba287cfa64583eeb789bd7f6825e0006e28ca
Gokul Suresh s.v <gokulofficial18062@gmail.com>
2024-04-09 00:44:27 +0530

    sub or nested routes

---
+++ Routing/src/app/complex-dashboard/@notification/archieved/page.tsx
@@ -0,0 +1,8 @@
+import Card from "@/components/card";
+import Link from "next/link";
+
+export default function archivedNotification(){
+    return <Card>archivedNotification dashboard
+        <h1> <Link href='/complex-dashboard'>default</Link></h1>
+    </Card>
+}
--- Routing/src/app/complex-dashboard/@notification/page.tsx
+++ Routing/src/app/complex-dashboard/@notification/page.tsx
@@ -1,5 +1,8 @@
 import Card from "@/components/card";
+import Link from "next/link";
 
 export default function notificationDashboard(){
-    return <Card>notification dashboard</Card>
+    return <Card>notification dashboard
+        <h1> <Link href='/complex-dashboard/archieved'>Archived</Link></h1>
+    </Card>
 }
