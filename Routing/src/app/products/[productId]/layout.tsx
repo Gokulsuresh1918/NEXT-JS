8e7e46ab6d6e8bd965c05bdf654811440a194137
Gokul Suresh s.v <gokulofficial18062@gmail.com>
2024-04-08 18:47:58 +0530

    routing metadatra

---
+++ Routing/src/app/products/[productId]/layout.tsx
@@ -0,0 +1,14 @@
+
+  export default function productdetails({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <>
+      {children}
+      <h1>featured product curoseal</h1>
+      </>
+    )
+  }
+  
--- Routing/src/app/products/[productId]/page.tsx
+++ Routing/src/app/products/[productId]/page.tsx
@@ -1,9 +1,24 @@
-export default function productdetails({
-  params,
-}: {
+import { metadata } from "@/app/layout";
+import { Metadata } from "next";
+import { title } from "process";
+
+type props = {
   params: {
     productId: string;
   };
-}) {
+};
+export const generateMetadata = async ({
+  params,
+}: props): Promise<Metadata> => {
+  const title = await new Promise((res, rej) => {
+    setTimeout(() => {
+      res(`i phome ${params.productId}`);
+    }, 1000);
+  });
+  return {
+    title: `Product ${title}`,
+  };
+};
+export default function productdetails({ params }: props) {
   return <h1>product details{params.productId}</h1>;
 }
