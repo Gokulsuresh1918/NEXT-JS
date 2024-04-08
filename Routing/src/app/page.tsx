5f6cdbce17500a6fb4d8ae3bfbe3ea4dc5068adf
Gokul Suresh s.v <gokulofficial18062@gmail.com>
2024-04-08 19:17:32 +0530

    link component navigation

--- Routing/src/app/page.tsx
+++ Routing/src/app/page.tsx
@@ -1,3 +1,12 @@
+
+import Link from "next/link"
+
+
 export default function Home(){
-    return <h1>Welcome home</h1>
+    return (
+        <>
+        <h1>Welcome to home</h1>
+        <h1><Link href='/about'>About</Link> </h1>
+        </>
+    )
 }
--- Routing/src/app/products/page.tsx
+++ Routing/src/app/products/page.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link"
+
 export default function products(){
+    const id=377
     return (
         <>
+        <Link href='/'><h1>home</h1></Link>
         <h2>products details</h2>
-        <h1>1</h1>
-        <h1>2</h1>
+        <h1><Link href='/products/1'>1</Link> </h1>
+        <h1><Link href='/products/2'>2</Link> </h1>
+        <h1><Link href={`products/${id}`}>this is {id}</Link> </h1>
         </>
     )
 }
