e5ac8c8d8c88390208a5444863ec9b921a83c8d8
Gokul Suresh s.v <gokulofficial18062@gmail.com>
2024-04-08 11:53:54 +0530

    dynamic routes

---
+++ Routing/src/app/products/[productId]/page.tsx
@@ -0,0 +1,7 @@
+
+
+export default function productdetails({ params }:{params:{
+    productId:string
+}}) {
+  return <h1>product details{params.productId}</h1>;
+}
---
+++ Routing/src/app/products/page.tsx
@@ -0,0 +1,9 @@
+export default function products(){
+    return (
+        <>
+        <h2>products details</h2>
+        <h1>1</h1>
+        <h1>2</h1>
+        </>
+    )
+}
