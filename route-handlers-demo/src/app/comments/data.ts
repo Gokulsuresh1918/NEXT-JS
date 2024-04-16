61918a0dee750234e7ffb7990f71d017b7705085
Gokul Suresh s.v <gokulofficial18062@gmail.com>
2024-04-16 20:09:17 +0530

    handleing get request

--- route-handlers-demo/src/app/comments/data.ts
+++ route-handlers-demo/src/app/comments/data.ts
@@ -1,5 +1,5 @@
 export const comments=[
     {id:1,text:'first  comments'},
-    {id:2,text:'second  comments'}
-    {id:3,text:'third  comments'}
+    {id:2,text:'second  comments'},
+    {id:3,text:'third  comments'},
 ]
