c93cbd03dd6792f4cbe7c4897a42a56a70d0cbb9
Gokul Suresh s.v <gokulofficial18062@gmail.com>
2024-04-17 12:36:35 +0530

    querrry params

--- route-handlers-demo/src/app/comments/[id]/route.ts
+++ route-handlers-demo/src/app/comments/[id]/route.ts
@@ -1,14 +1,16 @@
+import { NextRequest } from "next/server";
 import { comments } from "../data";
 //get request
-export async function GET(
-  request: Request,
-  { params }: { params: { id: string } }
-) {
-  const comment = comments.find(
-    (comment) => comment.id === parseInt(params.id)
-  );
-  return Response.json(comment);
-}
+// export async function GET(
+//   request: Request,
+//   { params }: { params: { id: string } }
+// ) {
+//   const comment = comments.find(
+//     (comment) => comment.id === parseInt(params.id)
+//   );
+//   return Response.json(comment);
+// }
+
 //patch request
 export async function PATCH(
   request: Request,
@@ -30,7 +32,18 @@ export async function DELETE(
   const index = comments.findIndex(
     (comment) => comment.id === parseInt(params.id)
   );
-const deleted=comments[index]
-comments.splice(index,1)
+  const deleted = comments[index];
+  comments.splice(index, 1);
   return Response.json(deleted);
+}
+//querry params
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("query");
+  const filteredcomment = query
+    ? comments.filter((comment) => {
+        comment.text.includes(query);
+      })
+    : comments;
+  return Response.json(filteredcomment);
 }
