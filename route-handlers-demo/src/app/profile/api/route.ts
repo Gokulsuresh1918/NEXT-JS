a5c8c98a282532ab1d646fed803a13a592e443b8
Gokul Suresh s.v <gokulofficial18062@gmail.com>
2024-04-17 13:46:47 +0530

    setting of cookie

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,8 +1,17 @@
-import { headers } from "next/headers"
+import { headers, cookies } from "next/headers";
 
 export async function GET() {
-    const headerList=headers()
-    console.log(headerList.get('Authorization'));
-    
-    return new Response('GET handlers')
-  }
+//setting of cookie
+  cookies().set("theme", "dark");
+  console.log(cookies().get("theme"));
+
+  //setting of header
+  const headerList = headers();
+  console.log(headerList.get("Authorization"));
+
+  return new Response("<h1>this is new </h1>", {
+    headers: {
+      "Content-Type": "text/html",
+    },
+  });
+}
