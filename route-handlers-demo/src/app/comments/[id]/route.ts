e1b83d59e98874b63726e9fe1985914319bf0804
Gokul Suresh s.v <gokulofficial18062@gmail.com>
2024-04-17 12:19:26 +0530

    delete request

--- route-handlers-demo/src/app/comments/[id]/route.ts
+++ route-handlers-demo/src/app/comments/[id]/route.ts
@@ -1,4 +1,5 @@
 import { comments } from "../data";
+//get request
 export async function GET(
   request: Request,
   { params }: { params: { id: string } }
@@ -8,6 +9,7 @@ export async function GET(
   );
   return Response.json(comment);
 }
+//patch request
 export async function PATCH(
   request: Request,
   { params }: { params: { id: string } }
@@ -17,6 +19,18 @@ export async function PATCH(
   const index = comments.findIndex(
     (comment) => comment.id === parseInt(params.id)
   );
-  comments[index].text=text
-  return Response.json(comments[index])
+  comments[index].text = text;
+  return Response.json(comments[index]);
+}
+// delete request
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const index = comments.findIndex(
+    (comment) => comment.id === parseInt(params.id)
+  );
+const deleted=comments[index]
+comments.splice(index,1)
+  return Response.json(deleted);
 }
